17b15f25ce8d9048b23b758f1c8f841b3addda16
Guibas <bublitz.stein@gmail.com>
2018-03-26 15:35:59 -0300

    Add exercises 35 and 36

---
+++ exercicio35.js
@@ -0,0 +1,6 @@
+const data = new Date();
+const dia = data.getDate();
+const mes = data.getMonth() + 1;
+const ano = data.getFullYear();
+
+alert(dia + '/' + mes + '/' + ano);
---
+++ exercicio36.js
@@ -0,0 +1,10 @@
+const data = new Date();
+
+let meses = ['jan', 'fev', 'marc', 'abr', 'may',
+'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
+
+
+const mes = data.getMonth();
+
+
+alert(meses[mes]);
